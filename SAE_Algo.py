f063b57a16ff31520a194f124fae162be3f4ca2f
Nathan <nathan.liegeon@outlook.com>
2024-01-11 00:44:52 +0100

    Début de doc + Modification fonction de test + Décomptage opérations des fonctions d'ajout de retard :skull:

--- SAE_Algo.py
+++ SAE_Algo.py
@@ -76,47 +76,54 @@ def filtreParDichotomie(tab: [int], val: int) -> ([int]):
 # Algos de tri
 
 def fusion(tab,iDeb,iFin):
+    """
+    
+
+    Args:
+        tab (_type_): _description_
+        iDeb (_type_): _description_
+        iFin (_type_): _description_
+    """
     global cpt
-    temp = zeros(iFin - iDeb + 1,'i') # 1
-    iMilieu = (iDeb + iFin)//2 # 3
-    i = iDeb # 1
-    j = iMilieu + 1 # 2
-    k = 0 # 1
+    temp = zeros(iFin - iDeb + 1,'i')
+    iMilieu = (iDeb + iFin)//2 
+    i = iDeb 
+    j = iMilieu + 1 
+    k = 0 
     cpt = cpt+8
-    while (i <= iMilieu and j <= iFin): # 3
-        if (tab[i] < tab[j]): # 3
-            temp[k] = tab[i] # 3
-            i = i + 1 # 2
+    while (i <= iMilieu and j <= iFin):
+        if (tab[i] < tab[j]): 
+            temp[k] = tab[i] 
+            i = i + 1 
             cpt = cpt+5
         else:
-            temp[k] = tab[j] # 3
-            j = j + 1 # 2
+            temp[k] = tab[j] 
+            j = j + 1 
             cpt = cpt+5
-        k = k + 1 # 2
+        k = k + 1 
         cpt = cpt+6
     cpt = cpt+3
-    while (i <= iMilieu): # 1
+    while (i <= iMilieu): 
         cpt = cpt+8
-        temp[k] = tab[i] # 3
-        i = i + 1 # 2
-        k=k+1 # 2
+        temp[k] = tab[i] 
+        i = i + 1 
+        k=k+1 
     cpt = cpt+1
-    while (j <= iFin): # 1
-        temp[k] = tab[j] # 3
-        j = j + 1 # 2
-        k = k + 1 # 2
+    while (j <= iFin): 
+        temp[k] = tab[j] 
+        j = j + 1 
+        k = k + 1 
         cpt = cpt+8
     k = 0
     cpt = cpt+2
-    while k <= (iFin - iDeb): # 2
-        tab[iDeb + k] = temp[k] # 4
-        k = k + 1 # 2
+    while k <= (iFin - iDeb): 
+        tab[iDeb + k] = temp[k] 
+        k = k + 1 
         cpt = cpt+8
     cpt = cpt+2
         
         
-
-def triFusion(tab,i,j):
+def triFusionRec(tab,i,j):
     """
     """
     global cpt
@@ -124,15 +131,19 @@ def triFusion(tab,i,j):
     if i<j:
         m = (i+j)//2
         cpt = cpt+3
-        triFusion(tab,i,m)
-        triFusion(tab,m+1,j)
+        triFusionRec(tab,i,m)
+        triFusionRec(tab,m+1,j)
         fusion(tab,i,j)
+
+def triFusion(tab):
+    triFusionRec(tab, 0, len(tab)-1)
+    
 
 def echanger(tab,a,b):
     global cpt
-    tmp=tab[a] # 2
-    tab[a]=tab[b] # 3
-    tab[b]=tmp # 2
+    tmp=tab[a] 
+    tab[a]=tab[b] 
+    tab[b]=tmp 
     cpt = cpt+7
     
     
@@ -140,26 +151,24 @@ def tabMin(tab,i):
     global cpt
     ind_min=i
     cpt = cpt+1
-    while i<len(tab): #2
-        if tab[i]<tab[ind_min]: #3
+    while i<len(tab): 
+        if tab[i]<tab[ind_min]:
             ind_min=i
             cpt = cpt + 1
-        i = i+1 # 2
+        i = i+1 
         cpt = cpt+7
     cpt = cpt+2
     return ind_min
 
 
 def triSelection(tab):
-    """
-    """
     global cpt 
     cpt = cpt+2
     i = 0
     ind_min = 0
     while i<len(tab) - 1:
         ind_min = tabMin(tab, i)
-        if ind_min != i: # 1
+        if ind_min != i: 
             echanger(tab, ind_min, i)
         i = i + 1
         cpt = cpt + 3
@@ -174,12 +183,12 @@ def triInsertion(tab: [int]):
             if tab[t] < tab [t - 1]:
                 echanger(tab, t, t - 1)
                 t = t - 1
-                cpt = cpt + 8 + 1 #
+                cpt = cpt + 8 + 1 
             else:
                 trie = True
-                cpt = cpt + 1 #
+                cpt = cpt + 1 
         i = i + 1
-        cpt = cpt + 3 + 1 #
+        cpt = cpt + 3 + 1 
 
 def tabMax(tab,i):
     global cpt
@@ -199,10 +208,10 @@ def renverser(tab,i):
     global cpt
     j = len(tab)-1
     cpt = cpt + 3
-    while i<j: # 1
+    while i<j:
         echanger(tab,i,j) 
-        i = i+1 # 2
-        j = j-1 # 2
+        i = i+1 
+        j = j-1
         cpt = cpt+5
     cpt = cpt+1
 
@@ -211,7 +220,7 @@ def triCrepe(tab):
     global cpt
     i=0
     cpt = cpt + 1
-    while i<len(tab): #2
+    while i<len(tab):
         ind_max=tabMax(tab,i)
         renverser(tab,ind_max)
         renverser(tab,i)
@@ -229,22 +238,24 @@ def copier(tab):
 
 def rechercheLinéaire(tab,val):
     """
-    entrée: tab:[int], val:int ;
-    pré-cond: None ;
-    sortie: i_val,cpt:int ;
-    post-cond: i_val vaut l'indice de la 1ere occurrence de la valeur recherchée,
-    -1 si elle n'est pas dans le tableau
-    cpt est le nombre d'opérations élémentaires effectuées ;
+    Renvoie la 1ere occurrence de val en effectuant une recherche linéaire, -1 sinon
+
+    Args:
+        tab ([int]): Tableau non vide d'entiers quelconque
+        val (int): Valeur à trouver
+    
+    Returns:
+        int: indice de la 1ere occurrence de val, -1 si elle n'est pas dedans
     """
     global cpt
     i = 0
     i_val = -1
-    cpt = cpt + 2 # 2 opérations élémentaires avant la boucle
-    while i<len(tab) and i_val<0 : # 1 requête, 3 comparaisons
-        if tab[i] == val: # 1 requêtes et une comparaison
-            i_val = i # 1 affectation
+    cpt = cpt + 2 
+    while i<len(tab) and i_val<0 :
+        if tab[i] == val: 
+            i_val = i 
             cpt = cpt+1
-        i = i+1 # 1 opération et 1 affectation
+        i = i+1 
         cpt = cpt+9
     cpt = cpt+2
     return i_val
@@ -253,12 +264,14 @@ def rechercheLinéaire(tab,val):
 
 def rechercheDichotomie(tab: [int], val: int) -> (int):
     """
-    entrée: tab:[int], val:int ;
-    pré-cond: None ;
-    sortie: i_val,cpt:int ;
-    post-cond: i_val vaut l'indice d'une des occurences de la valeur recherchée,
-    -1 si elle n'est pas dans le tableau
-    cpt est le nombre d'opérations élémentaires effectuées ;
+    Renvoie l'indice d'une des occurrences de val si val est dans le tableau, -1 sinon en effectuant une recherche par Dichotomie
+
+    Args:
+        tab ([int]): Tableau non vide d'entiers trié par ordre croissant
+        val (int): Valeur à trouver
+
+    Returns:
+        int: 1er Indice de val trouvé (pas nécessairement sa 1ere occurrence )
     """
     global cpt
     i = 0
@@ -285,12 +298,15 @@ def rechercheDichotomie(tab: [int], val: int) -> (int):
 
 def rechercheBalayage(tab, val):
     """
-    entrée: tab:[int], val:int ;
-    pré-cond: None ;
-    sortie: i_val,cpt:int ;
-    post-cond: i_val vaut l'indice d'une des occurences de la valeur recherchée,
-    -1 si elle n'est pas dans le tableau
-    cpt est le nombre d'opérations élémentaires effectuées ;
+    Renvoie l'indice d'une des occurrences de val si val est dans le tableau, -1 sinon en effectuant une recherche par Balayage.
+    S'occupe de faire le 1er appel à la fonction rechercheBalayageRec.
+
+    Args:
+        tab ([int]): Tableau non vide d'entiers trié par ordre croissant
+        val (int): Valeur à trouver
+
+    Returns:
+        int: 1er Indice de val trouvé (pas nécessairement sa 1ere occurrence )
     """
     global cpt
     pas = (len(tab)-1)//4
@@ -300,8 +316,7 @@ def rechercheBalayage(tab, val):
 
 def getIndice(iFin, i, j, pas):
     """
-    Renvoie le prochaine indice à traiter pour la fonction
-    de recherche par balayage
+    Renvoie le prochaine indice à traiter pour la fonction rechercheBalayageRec
 
     Args:
         iFin (int): Indice maximum possible
@@ -325,7 +340,7 @@ def getIndice(iFin, i, j, pas):
  
 def rechercheBalayageRec(tab, val, pas, iDeb, iFin):
     """
-    Renvoie l'indice de val dans le tableau
+    Renvoie l'indice de val dans le tableau récursivement, -1 si il n'y est pas
 
     Args:
         tab ([int]): Tableau dans lequel on effectue la recherche
@@ -378,22 +393,6 @@ def echange_suivant(tab,indice):
     tab[indice+1] = tab[indice]
     tab[indice] = tmp
 
-
-def ecartHeures(h1,h2):
-    global cpt 
-    heureEcart = h1//100 - h2//100
-    minuteEcart = h1%100 - h2%100
-    
-    cpt = cpt + 9
-    if minuteEcart < 0 :
-        minuteEcart = minuteEcart + 60
-        heureEcart = heureEcart - 1
-        
-        cpt = cpt + 4
-    
-    return heureEcart*60 + minuteEcart
-        
-
 def decalerJusqua(tab,indiceDebut,indiceFin):
     global cpt
     temp = tab[indiceDebut]
@@ -407,8 +406,43 @@ def decalerJusqua(tab,indiceDebut,indiceFin):
         cpt = cpt + 6
     tab[indiceFin] = temp
     cpt = cpt +  4
+
+def ecartHeures(h1,h2):
+    """
+    Renvoie l'écart en minutes entre les 2 heures
+    Il sera négatif si h2 > h1
+
+    Args:
+        h1 (int): Stockée sous la forme hhmm (1205 = 12h05), 0 <= h1 < 2400
+        h2 (int): Stockée sous la forme hhmm (1205 = 12h05), 0 <= h2 < 2400
+
+    Returns:
+        int: écart en minutes entre les 2.
+    """
+    global cpt 
+    heureEcart = h1//100 - h2//100
+    minuteEcart = h1%100 - h2%100
+    
+    cpt = cpt + 9
+    if minuteEcart < 0 :
+        minuteEcart = minuteEcart + 60
+        heureEcart = heureEcart - 1
+        
+        cpt = cpt + 4
+    
+    return heureEcart*60 + minuteEcart
     
 def ajoutHeure(heure1, val):
+    """
+    Ajoute val minutes à l'heure
+
+    Args:
+        heure1 (int): Stockée sous la forme hhmm (1205 = 12h05), 0 <= heure1 < 2400
+        val (int): Nombre de minutes à ajouter (ici 90 = 1h30)
+
+    Returns:
+        int: Heure après que val minutes soient passées (l'appel ajoutHeure(1205,90) renvoie 1335)
+    """
     global cpt
     heure = heure1//100
     minute = heure1%100
@@ -425,73 +459,113 @@ def ajoutHeure(heure1, val):
 
 
 def ajoutRetard(tab, indiceVolRetarde, tpsRetard):
+    """
+    Retarde un vol de tpsRetard minutes de telle sorte à ce que:
+        - Il ait au maximum 60 minutes de retard
+        - Il y ait 5 minutes d'écart entre chaque vol
+        - Le retard soit le plus bas possible
+        - La liste reste triée après usage de la fonction
+    Si le vol n'a pas pu être retardé en respectant ces consignes, le vol est annulé:
+        - Son heure est mise à -1
+        - Il est déplacé à la fin du tableau
+
+    Args:
+        tab ([int]): Tableau d'heures représentant la liste de vols, triée par ordre croissant
+        indiceVolRetarde (int): Indice du vol retardé dans le tableau d'heures
+        tpsRetard (int): Retard en minutes à appliquer au vol
+
+    Returns:
+        int: Retard appliqué au vol par la fonction, -1 si il est annulé
+    """
     global cpt
     
+    cpt = cpt + 5
     heureMin = ajoutHeure(tab[indiceVolRetarde], tpsRetard)
     heureMax = ajoutHeure(tab[indiceVolRetarde], 60)
     if heureMax > 2200:
+        cpt = cpt + 1
         heureMax = 2200
     
+    cpt = cpt + 6
     retardAccumule = tpsRetard
     indiceVol = indiceVolRetarde
     place = False
-    
-    
-    if len(tab) > 1:
-        if indiceVolRetarde == len(tab) - 1:
+    if indiceVolRetarde == len(tab) - 1:
+        if tpsRetard <=60:
             tab[indiceVolRetarde] = heureMin
             return tpsRetard
-            
-        while indiceVol + 1 < len(tab) and tab[indiceVol+1] < heureMin:
-            indiceVol = indiceVol + 1
-
-        heureActuelle = heureMin
-        while not place and retardAccumule <= 60 and heureActuelle <= heureMax:
-            if indiceVol > indiceVolRetarde :
-                ecart1 = ecartHeures(heureActuelle, tab[indiceVol])
-                if ecart1 >= 5:
-                    place = True
-                    
-            if indiceVol + 1 < len(tab) :
-                ecart2 = ecartHeures(tab[indiceVol + 1], heureActuelle)
-                if ecart2 >= 5:
-                    if indiceVol > indiceVolRetarde:
-                        if ecart1 >= 5:
-                            place = True
-                        else:
-                            place = False
-                else:
-                    place = False
-    
-            
-            if not place :
-                if indiceVol + 1 < len(tab):
-                    if ecart2 == 0 :
-                        indiceVol = indiceVol + 1
-                        retardAccumule = retardAccumule + 5
-                        heureActuelle = ajoutHeure(heureActuelle, 5)
-                        
-                    else:
-                        retardAccumule = retardAccumule + ecart2
-                        heureActuelle = ajoutHeure(heureActuelle, ecart2)
-                
-                else:
-                    retardAccumule = retardAccumule + 1
-                    heureActuelle = ajoutHeure(heureActuelle, 1)
-    
-        if place:
-            tab[indiceVolRetarde] = heureActuelle
-            decalerJusqua(tab, indiceVolRetarde, indiceVol)
-        
         else:
             tab[indiceVolRetarde] = -1
-            retardAccumule = -1
-            decalerJusqua(tab, indiceVolRetarde, len(tab)-1)
+            return -1
+            
+        
+    while indiceVol + 1 < len(tab) and tab[indiceVol+1] < heureMin:
+        indiceVol = indiceVol + 1
+        cpt = cpt + 9
+        
+    heureActuelle = heureMin
+    cpt = cpt + 8
 
-    else:
-        tab[indiceVolRetarde] = tab[indiceVolRetarde] + tpsRetard
-        retardAccumule = tpsRetard
+    while not place and retardAccumule <= 60 and heureActuelle <= heureMax:
+        cpt = cpt + 6
+        if indiceVol > indiceVolRetarde :
+            cpt = cpt + 3
+            ecart1 = ecartHeures(heureActuelle, tab[indiceVol])
+            cpt = cpt + 1
+            if ecart1 >= 5:
+                cpt = cpt + 1
+                place = True
+        
+        cpt = cpt + 3
+        if indiceVol + 1 < len(tab) :
+            cpt = cpt + 5
+            ecart2 = ecartHeures(tab[indiceVol + 1], heureActuelle)
+            if ecart2 >= 5:
+                cpt = cpt + 1
+                if indiceVol > indiceVolRetarde:
+                    cpt = cpt + 2   
+                    if ecart1 >= 5:
+                        place = True
+                    else:
+                        place = False
+            else:
+                cpt = cpt + 1
+                place = False
+
+        cpt = cpt + 1
+        if not place :
+            cpt = cpt + 3
+            if indiceVol + 1 < len(tab):
+                cpt = cpt + 1
+                if ecart2 == 0 :
+                    indiceVol = indiceVol + 1
+                    retardAccumule = retardAccumule + 5
+                    heureActuelle = ajoutHeure(heureActuelle, 5)
+                    cpt = cpt + 6
+                    
+                else:
+                    retardAccumule = retardAccumule + ecart2
+                    heureActuelle = ajoutHeure(heureActuelle, ecart2)
+                    cpt = cpt + 4
+            
+            else:
+                retardAccumule = retardAccumule + 1
+                heureActuelle = ajoutHeure(heureActuelle, 1)
+                cpt = cpt + 4
+
+    cpt = cpt + 6
     
+    if place:
+        tab[indiceVolRetarde] = heureActuelle
+        decalerJusqua(tab, indiceVolRetarde, indiceVol)
+        cpt = cpt + 3
+    
+    else:
+        tab[indiceVolRetarde] = -1
+        retardAccumule = -1
+        decalerJusqua(tab, indiceVolRetarde, len(tab)-1)
+        cpt = cpt + 6
+
     return retardAccumule
 
 # Fonction de test  
@@ -550,7 +624,7 @@ def test():
                 print(f"Indice de la valeur : {rechercheLinéaire(tab, val)}, Nombre d'opérations : {cpt}")
             else:
                 tri = entreeUtilisateur("Inclure le tri dans le décompte des opérations (le tri fusion sera utilisé par défaut) ?\n1- Oui\n2- Non\n=> ", 1, 2)
-                triFusion(tab, 0, taille-1)
+                triFusion(tab)
                 if tri == 2:
                     cpt = 0
             if choix == 2:
@@ -574,7 +648,7 @@ def test():
             
             else:
                 tri = entreeUtilisateur("Inclure le tri dans le décompte des opérations (le tri fusion sera utilisé par défaut) ?\n1- Oui\n2- Non\n=> ", 1, 2)
-                triFusion(tab, 0, taille-1)
+                triFusion(tab)
                 if tri == 2:
                     cpt = 0
                 print(f"Indice de la valeur : {filtreParDichotomie(tab, val)}, Nombre d'opérations : {cpt}")
@@ -600,7 +674,7 @@ def test():
             if choix == 3:
                 triCrepe(tab)
             if choix ==4:
-                triFusion(tab, 0, taille - 1)
+                triFusion(tab)
             print(tab)
             print(f"Nombre d'opérations : {cpt}")
             conserver = (entreeUtilisateur("Souhaitez refaire des tests sur le même tableau ?\n1 - Oui\n2- Non\n=> ", 1, 2)-1)*2
@@ -626,7 +700,7 @@ def test():
             print(f"Avant : {copyTab}")
             print(f"Après : {tab}")
             if retard >= 0:
-                print(f"Le vol a été retardé de {retard} minutes")
+                print(f"Le vol a été retardé de {retard} minutes.\nNombre d'opérations : {cpt}")
             else:
                 print("Le vol n'a pas pu être placé et a été annulé")
             
@@ -644,6 +718,5 @@ def test():
 # Tests
 
 cpt = 0
-
 test()
             
